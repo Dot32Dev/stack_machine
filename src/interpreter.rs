72ea08c4be054a2da05882386a8447ffa789a328
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2025-02-11 20:33:34 +0800

    Stack machine GUI

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,8 +1,9 @@
 use crate::token::Instruction;
 
 pub struct Interpreter {
-    instructions: Vec<Instruction>,
-    stack: Vec<i32>,
+    pub instructions: Vec<Instruction>,
+    pub stack: Vec<i32>,
+    pub instruction_pointer: usize,
 }
 
 impl Interpreter {
@@ -10,39 +11,46 @@ impl Interpreter {
         Self {
             instructions,
             stack: Vec::new(),
+            instruction_pointer: 0,
         }
     }
 
-    pub fn run(&mut self) -> i32 {
-        for instruction in &self.instructions {
-            match instruction {
-                Instruction::Push(number) => {
-                    self.stack.push(*number);
-                }
-                Instruction::Pop => {
-                    self.stack.pop();
-                }
-                Instruction::Add => {
-                    let num2 = self.stack.pop().unwrap();
-                    let num1 = self.stack.pop().unwrap();
-                    self.stack.push(num1 + num2);
-                }
-                Instruction::Subtract => {
-                    let num2 = self.stack.pop().unwrap();
-                    let num1 = self.stack.pop().unwrap();
-                    self.stack.push(num1 - num2);
-                }
-                Instruction::Multiply => {
-                    let num2 = self.stack.pop().unwrap();
-                    let num1 = self.stack.pop().unwrap();
-                    self.stack.push(num1 * num2);
-                }
-                Instruction::Divide => {
-                    let num2 = self.stack.pop().unwrap();
-                    let num1 = self.stack.pop().unwrap();
-                    self.stack.push(num1 / num2);
-                }
+    pub fn next(&mut self) {
+        match self.instructions[self.instruction_pointer] {
+            Instruction::Push(number) => {
+                self.stack.push(number);
             }
+            Instruction::Pop => {
+                self.stack.pop();
+            }
+            Instruction::Add => {
+                let num2 = self.stack.pop().unwrap();
+                let num1 = self.stack.pop().unwrap();
+                self.stack.push(num1 + num2);
+            }
+            Instruction::Subtract => {
+                let num2 = self.stack.pop().unwrap();
+                let num1 = self.stack.pop().unwrap();
+                self.stack.push(num1 - num2);
+            }
+            Instruction::Multiply => {
+                let num2 = self.stack.pop().unwrap();
+                let num1 = self.stack.pop().unwrap();
+                self.stack.push(num1 * num2);
+            }
+            Instruction::Divide => {
+                let num2 = self.stack.pop().unwrap();
+                let num1 = self.stack.pop().unwrap();
+                self.stack.push(num1 / num2);
+            }
+        }
+
+        self.instruction_pointer += 1;
+    }
+
+    pub fn run(&mut self) -> i32 {
+        for _ in 0..self.instructions.len() {
+            self.next();
         }
 
         self.stack.pop().unwrap()
--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,122 @@
-use eframe::{egui, wgpu::naga::Interpolation};
+use eframe::egui;
+use egui::Color32;
+use egui::RichText;
 use interpreter::Interpreter;
 use parser::Parser;
 use std::fs;
+use std::time::{Duration, Instant};
+use token::Instruction;
 
 mod interpreter;
 mod lexer;
 mod parser;
 mod token;
 
-// struct MyApp {
-//     name: String,
-//     age: u32,
-// }
+struct MyApp {
+    interpreter: Interpreter,
+    last_update: Instant,
+    source: String,
+}
 
-// impl eframe::App for MyApp {
-//     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-//         egui::CentralPanel::default().show(ctx, |ui| {
-//             ui.heading("My egui Application");
-//             ui.horizontal(|ui| {
-//                 let name_label = ui.label("Your name: ");
-//                 ui.text_edit_singleline(&mut self.name)
-//                     .labelled_by(name_label.id);
-//             });
-//             ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-//             if ui.button("Increment").clicked() {
-//                 self.age += 1;
-//             }
-//             ui.label(format!("Hello '{}', age {}", self.name, self.age));
-//         });
-//     }
-// }
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        ctx.request_repaint();
+
+        let now = Instant::now();
+        if now.duration_since(self.last_update) >= Duration::from_millis(1000) {
+            if self.interpreter.instructions.len()
+                > self.interpreter.instruction_pointer
+            {
+                self.interpreter.next();
+                self.last_update = now;
+            }
+        }
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Expression");
+            ui.label(&self.source);
+            ui.add_space(10.0);
+            ui.heading("Stack Machine");
+            ui.columns(2, |cols| {
+                // for (i, col) in cols.iter_mut().enumerate() {
+                //     // col.label(format!("Column {} out of {}", i + 1, 2));
+                //     // // if i + 1 == 2 && col.button("Delete this").clicked() {
+                //     // //     //
+                //     // // }
+                //     if
+                // }
+                cols[0].label("Instructions");
+                cols[0].add_space(10.0);
+                for (i, instruction) in
+                    self.interpreter.instructions.iter().enumerate()
+                {
+                    let string = match instruction {
+                        Instruction::Push(number) => {
+                            format!("Push( {} )", number)
+                        }
+                        Instruction::Pop => String::from("Pop"),
+                        Instruction::Add => String::from("Add"),
+                        Instruction::Subtract => String::from("Subtract"),
+                        Instruction::Multiply => String::from("Multiply"),
+                        Instruction::Divide => String::from("Divide"),
+                    };
+                    if i == self.interpreter.instruction_pointer {
+                        cols[0].label(
+                            RichText::new(string)
+                                .color(Color32::from_rgb(110, 255, 110)),
+                        );
+                    } else {
+                        cols[0].label(string);
+                    }
+                }
+
+                cols[1].label("Stack");
+                cols[1].add_space(10.0);
+                for number in &self.interpreter.stack {
+                    cols[1].label(number.to_string());
+                }
+            });
+            // ui.horizontal(|ui| {
+            //     let name_label = ui.label("Your name: ");
+            //     ui.text_edit_singleline(&mut self.name)
+            //         .labelled_by(name_label.id);
+            // });
+            // ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            // if ui.button("Increment").clicked() {
+            //     self.age += 1;
+            // }
+            // ui.label(format!("Hello '{}', age {}", self.name, self.age));
+        });
+    }
+}
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Can't read file");
     let mut parser = Parser::new(&input);
     parser.expr();
     let mut interpreter = Interpreter::new(parser.instructions);
-    let answer = interpreter.run();
-    println!("{}", answer);
+    // let answer = interpreter.run();
+    // println!("{}", answer);
 
-    // let options = eframe::NativeOptions {
-    //     viewport: egui::ViewportBuilder::default()
-    //         .with_inner_size([320.0, 240.0]),
-    //     ..Default::default()
-    // };
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([320.0, 280.0]),
+        ..Default::default()
+    };
 
-    // eframe::run_native(
-    //     "My egui App",
-    //     options,
-    //     Box::new(|cc| {
-    //         // This gives us image support:
-    //         // egui_extras::install_image_loaders(&cc.egui_ctx);
+    eframe::run_native(
+        "Stack machine",
+        options,
+        Box::new(|_cc| {
+            // This gives us image support:
+            // egui_extras::install_image_loaders(&cc.egui_ctx);
 
-    //         Ok(Box::new(MyApp {
-    //             name: String::new(),
-    //             age: 0,
-    //         }))
-    //     }),
-    // )
-    // .unwrap();
+            Ok(Box::new(MyApp {
+                interpreter,
+                last_update: Instant::now(),
+                source: input,
+            }))
+        }),
+    )
+    .unwrap();
 }
